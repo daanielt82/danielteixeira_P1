46d4a7c4973cf3371699d76d0052fe349ee0738b
DanielTeixeira <danielima123@hotmail.com>
2019-10-02 13:28:46 -0300

    Questao 4

---
+++ q4.py
@@ -0,0 +1,7 @@
+def nob (num_a, num_b):
+    auxiliar1 = num_a//10
+    auxiliar2 = num_b//10
+    auxiliar3 = num_a - (auxiliar1*10)
+    auxiliar4 = num_b - (auxiliar2*10)
+    resultado = auxiliar1 + auxiliar2 + auxiliar3 + auxilixar4
+    print(resultado)
