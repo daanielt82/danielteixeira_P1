5576fe163589ebd6a31e22d369ecc9440df885c8
DanielTeixeira <danielima123@hotmail.com>
2019-10-02 13:31:53 -0300

    Questao 1 resolvida

---
+++ q1resolvida.py
@@ -0,0 +1,21 @@
+def raizes():
+    a = int(input("Digite o 'a': "))
+    b = int(input("Digite o 'b': "))
+    c = int(input("Digite o 'c': "))
+    delta = (b*b) - (4*a*c)
+    quadrada = delta**(1/2)
+    raiz1 = (b*-1 + quadrada) / 2*a
+    raiz2 = (b*-1 - quadrada) / 2*a
+    complexa = 0
+    if delta < 0:
+        complexa = 1
+        real = (b*-1)/2*a
+        imag = (quadrada*-1)/2*a
+        print(real)
+        print(imag)
+        return complexa
+    else:
+        complexa = 0
+        print(raiz1)
+        print(raiz2)
+        return complexa
